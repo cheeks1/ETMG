bcb0d9cd51f94f9e4abfbcc6d0f5c542d3830632
cheek <cheeks19@fullsail.edu>
2014-02-09 01:33:47 -0500

    Created the main menu scene and added Button to start

---
+++ GameProject1/GameProject1/Assets/mainMenu.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class mainMenu : MonoBehaviour {
+
+	// Use this for initialization
+	void OnGUI () {
+
+		Rect box = new Rect (10, 10, 100, 90);
+		GUI.Box (box, "Game Project 1");
+	
+		if (GUI.Button (new Rect (20, 40, 80, 20), "Start")) {
+			
+			Application.LoadLevel(1);
+			
+		}
+
+		//mExit = GUI.Button (Rect (20, 70, 80, 20), "Exit");
+	}
+
+}
