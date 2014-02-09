7270e9d13f77f1a0c6476454c062144b7a057909
cheek <cheeks19@fullsail.edu>
2014-02-09 02:51:43 -0500

    Modified
    Enlarged Main Menu text

--- GameProject1/GameProject1/Assets/mainMenu.cs
+++ GameProject1/GameProject1/Assets/mainMenu.cs
@@ -6,17 +6,32 @@ public class mainMenu : MonoBehaviour {
 	// Use this for initialization
 	void OnGUI () {
 
+		GUIStyle style = new GUIStyle();// = this.guiText;
+		style.fontSize = 100;
+		style.alignment = TextAnchor.UpperCenter;
+
 		Rect box = new Rect (10, 10, Screen.width - 20 , Screen.height - 20);
-		GUI.Box (box, "Game Project 1");
+		GUI.Box ((box), "Game Project 1", style);
 	
-		if (GUI.Button (new Rect (40, 40, Screen.width - 80, Screen.height/4 - 40), "Start")) 
+		//GUI.TextField(new Rect(10, 10, Screen.width - 20 , Screen.height - 20), "Game Project 1");
+		//Camera.current.rect
+
+		style.alignment = TextAnchor.MiddleCenter;
+		//style.border = this.guiTexture.border;
+
+		//style.border.left = 40;
+		//style.border.top = 400;
+		//style.border.bottom = Screen.height / 4 - 40;
+		//style.border.right = Screen.width - 80;
+
+		if (GUI.Button (new Rect (40, 400, Screen.width - 80, Screen.height/4 - 40), "Start", style)) 
 		{
 			
 			Application.LoadLevel(1);
 			
 		}
 
-		if(GUI.Button (new Rect (40, Screen.height/4 + 40, Screen.width - 80, Screen.height/4 - 40), "Exit"))
+		if(GUI.Button (new Rect (40, Screen.height/4 + 400, Screen.width - 80, Screen.height/4 - 40), "Exit", style))
 		{
 			Application.Quit();
 		}
