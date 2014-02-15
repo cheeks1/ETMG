7102908e77072c3f26568b82881e0678674332db
cheek <cheeks19@fullsail.edu>
2014-02-15 18:06:33 -0500

    Added
    Added particle emitter.
    Added accelerometer to enemies for moving them.

---
+++ GameProject1/GameProject1/Assets/EnemyMove.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyMove : MonoBehaviour {
+
+	private Vector3 mDir;
+	// Use this for initialization
+	void Start () {
+	
+		mDir = Vector3.zero;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Vector3 dir = Vector3.zero;
+		dir.x = -Input.acceleration.x;
+		dir.y = -Input.acceleration.y;
+
+		if (dir.sqrMagnitude > 1)
+			dir.Normalize();
+		
+		dir *= Time.deltaTime;
+
+		if (dir != mDir) 
+		{
+			mDir = dir;
+
+			transform.Translate (dir);
+		}
+	
+		Input.ResetInputAxes();
+
+	}
+}
---
+++ GameProject1/GameProject1/Assets/Speed.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Speed : MonoBehaviour {
+
+	private Vector3 mDir;
+
+	// Use this for initialization
+	void Start () {
+	
+		mDir = Vector3.zero;
+
+		GameObject.Find ("Particle System").particleEmitter.enabled = false;
+
+		GameObject.Find ("Particle System").particleEmitter.rndVelocity = mDir;
+
+		GameObject.Find ("Particle System").particleEmitter.worldVelocity = mDir;
+		
+		GameObject.Find ("Particle System").particleEmitter.localVelocity = mDir;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//particleEmitter emit = GameObject.Find ("Particle System").particleEmitter.worldVelocity = ;
+
+		Vector3 dir = Vector3.zero;
+		dir.x = -Input.acceleration.y;
+		dir.z = Input.acceleration.x;
+		if (dir.sqrMagnitude > 1)
+			dir.Normalize();
+		
+		dir *= Time.deltaTime;
+
+		if (dir != dir) 
+		{
+			GameObject.Find ("Particle System").particleEmitter.enabled = true;
+
+			GameObject.Find ("Particle System").particleEmitter.worldVelocity = dir;
+	
+			GameObject.Find ("Particle System").particleEmitter.localVelocity = dir;
+
+			GameObject.Find ("Particle System").particleEmitter.enabled = true;
+
+			mDir = dir;
+		}
+	}
+}
