fd21f8d7bb035ac5a1d9890fe2bfbe1606eccf2e
cheek <cheeks19@fullsail.edu>
2014-02-16 01:34:24 -0500

    Added
    Added GPS data.
    Use GPS data to change the color of the scene.
    R,G,B = Altitude, Longitude, Latitude

---
+++ GameProject1/GameProject1/Assets/GPS.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GPS : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+	{
+		Input.location.Start ();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		float fAlt = Input.location.lastData.altitude;
+		float fLong = Input.location.lastData.longitude;
+		float fLat = Input.location.lastData.latitude;
+
+		Vector3 col = new Vector3(fAlt, fLong, fLat);
+		col.Normalize();
+
+		this.GetComponent<GUITexture> ().color = new Color (col.x, col.y, col.z, 0.3f);
+	}
+
+	void Stop()
+	{
+		Input.location.Stop();
+	}
+}
--- GameProject1/GameProject1/Assets/Game_UI.cs
+++ GameProject1/GameProject1/Assets/Game_UI.cs
@@ -5,11 +5,15 @@ public class Game_UI : MonoBehaviour {
 
 	void OnGUI () 
 	{
-		GUI.Box (new Rect (0, 0, 100, 80),"");
-		
-		if (GUI.Button (new Rect (0, 0, 100, 80), "Main Menu")) 
+		GUIStyle style = new GUIStyle();
+		style.fontSize = 20;
+		style.alignment = TextAnchor.UpperCenter;
+
+		GUI.Box (new Rect (Screen.width - 100, Screen.height - 80, Screen.width - 40, Screen.height - 30),"");
+
+		if (GUI.Button (new Rect (Screen.width - 100, Screen.height - 80, Screen.width - 40, Screen.height - 30), "Main Menu", style)) 
 		{
-			Application.LoadLevel(0);
+			Application.LoadLevel(1);
 		}
 	}
 	// Use this for initialization
