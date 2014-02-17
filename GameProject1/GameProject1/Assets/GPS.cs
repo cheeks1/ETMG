330497f189984442b3b2b080fc2b026e82ed77be
cheek <cheeks19@fullsail.edu>
2014-02-16 22:52:01 -0500

    Added
    Added a box that shows GPS data while in the game

--- GameProject1/GameProject1/Assets/GPS.cs
+++ GameProject1/GameProject1/Assets/GPS.cs
@@ -3,27 +3,41 @@ using System.Collections;
 
 public class GPS : MonoBehaviour {
 
+	float fAlt;
+	float fLong;
+	float fLat;
 	// Use this for initialization
 	void Start () 
 	{
 		Input.location.Start ();
+		fAlt = 0.0f;
+		fLong = 0.0f;
+		fLat = 0.0f;
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
-		float fAlt = Input.location.lastData.altitude;
-		float fLong = Input.location.lastData.longitude;
-		float fLat = Input.location.lastData.latitude;
+		fAlt = Input.location.lastData.altitude;
+		fLong = Input.location.lastData.longitude;
+		fLat = Input.location.lastData.latitude;
 
 		Vector3 col = new Vector3(fAlt, fLong, fLat);
 		col.Normalize();
 
 		this.GetComponent<GUITexture> ().color = new Color (col.x, col.y, col.z, 0.3f);
+
+
 	}
 
 	void Stop()
 	{
 		Input.location.Stop();
+	}
+
+	void OnGUI()
+	{
+		GUI.Box (new Rect (0, 0, 400, 30),"Alt: " + fAlt + "; Long: " 
+		         + fLong + "; Lat: " + fLat);
 	}
 }
